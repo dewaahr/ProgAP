943aa04baaf2fecc94f9ab9f1437a98bd8beb2d0
hermanus rangga <hermanus.rangga@ti.ukdw.ac.id>
2024-10-31 01:02:11 +0700

    Notif Email

--- PresensiCek.py
+++ PresensiCek.py
@@ -1,8 +1,11 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import requests
 from bs4 import BeautifulSoup
 import json
 import os
 import time
+import smtplib
 # from Tes import check_user_data
 
 def get_presensi(html_content):
@@ -58,44 +61,67 @@ def get_kelas():
             link = link.replace('materi/index', 'id/kelas/presensi')
             links_presensi.append(link)
         return links_presensi
-                    
+    
+def create_Email(kelas,new_presensi):
+
+    kelas_file= json.load(open("datakelas.json"))
+    user = json.load(open("user.json"))
+    sender_email = user['email']
+    sender_password = user['passEmail']
+    recipient_email = user['recipient']
+
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = recipient_email
+    msg['Subject'] = "Presensi Baru Kelas " + kelas_file[kelas]
+
+    body = (
+        f"Tanggal: {new_presensi['Tanggal']}\n"
+        f"Mata Kuliah: {kelas_file[kelas]}\n"
+        f"Pertemuan Ke: {new_presensi['Pertemuan Ke']}\n"
+    )
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    try:
+        with smtplib.SMTP('smtp.gmail.com', 587) as server:
+            server.starttls()
+            server.login(sender_email, sender_password)
+            server.send_message(msg)
+        print("Email berhasil dikirim")
+        return True
+    except Exception as e:
+        print(f"Gagal mengirim email: {e}")
+        return False
 
 def main():
     list_kelas = get_kelas()
-    # print(list_kelas)
+    data_presensi = {}
     for i in list_kelas:
-        target_url = i
-        # target_url = "https://eclass.ukdw.ac.id/e-class/id/kelas/presensi/TI0043"
         login_url = "https://eclass.ukdw.ac.id/id/home/do_login"
+        # print(i)
         user = json.load(open("user.json"))
         payload = {
         'id': user['id'],
         'password': user['password'],
         'return_url': user['return_url']
         }
-
         with requests.Session() as session:
             response = session.post(login_url, data=payload)
-            if "Login gagal" in response.text:
-                print("Login gagal, silahkan periksa kembali ID atau password.")
-                return
-            else:
-                print("Login berhasil!")
-                response = session.get(target_url)
-                if response.ok:
-                    print("Berhasil mengakses halaman target!")
-                else:
-                    print("Gagal mengakses halaman target.")
-                    return
+            
+            response = session.get(i)
+ 
+            presensi = get_presensi(response.text)
+            data_presensi[i] = presensi
+    with open("dataPresensi.json", "r") as file:
+        data_presensi_lama = json.load(file)
+        for key in data_presensi_lama:
 
-                presensi = get_presensi(response.text)
-                print (presensi)
+                if data_presensi[key] != data_presensi_lama[key]:
+                    presensi_baru = data_presensi[key][0]
+                    create_Email(key,presensi_baru)
+
+                    json.dump(data_presensi, open("dataPresensi.json", "w"))
+                    break
         
-# while True:        
-#     presensiLama = main()
-#     time.sleep(10)
-#     presensiBaru = main()
-#     cek_presensi(presensiLama, presensiBaru)
-
-# get_kelas()
 main()
